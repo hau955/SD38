689f8ee557f8ca71f0c4c0ea8053f675f2e71f14
hauhd05 <hauhd9525@gmail.com>
2025-06-12 20:55:59 +0700

    first commit

---
+++ AppData/Models/ApplicationRole.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebModels.Models
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        [Required]
+        public DateTime NgayTao { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime NgaySua { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public bool TrangThai { get; set; } = true;
+
+        // Quan hệ 1 - N: 1 Role → N User
+        [JsonIgnore]
+        public virtual ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+    }
+}
