dc5c8197601b58ee3472ea5bde97397527839fce
hauhd05 <hauhd9525@gmail.com>
2025-07-05 03:12:26 +0700

    hau

---
+++ AppView/Areas/Admin/IRepo/IDanhMucRePo.cs
@@ -0,0 +1,13 @@
+﻿using AppView.Areas.Admin.ViewModels;
+
+namespace AppView.Areas.Admin.IRepo
+{
+    public interface IDanhMucRePo
+    {
+        Task<List<DanhMucViewModel>> GetAllDanhMucsAsync();
+        Task<DanhMucViewModel?> GetDanhMucByIdAsync(Guid id);
+        Task<bool> CreateDanhMucAsync(DanhMucViewModel model);
+        Task<bool> UpdateDanhMucAsync(DanhMucViewModel model);
+        Task<bool> DeleteDanhMucAsync(Guid id);
+    }
+}
